7abe7f214f95b3aba959d986b9f30eeec8be8a7f
Sumukha <sumukhahs95@gmail.com>
2021-05-22 23:27:40 +0530

    working version

---
+++ script.js
@@ -0,0 +1,45 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.querySelector('#nav');
+const toggleIcon = document.querySelector('#toggle-icon');
+const image1 = document.querySelector('#image1');
+const image2 = document.querySelector('#image2');
+const image3 = document.querySelector('#image3');
+const textBox = document.querySelector('#text-box');
+
+// Dark Mode Styles
+function darkmode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+    image1.src = 'img/undraw_proud_coder_dark.svg';
+    image2.src = 'img/undraw_feeling_proud_dark.svg';
+    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+}
+
+// Light Mode Styles
+function lightmode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    image1.src = 'img/undraw_proud_coder_light.svg';
+    image2.src = 'img/undraw_feeling_proud_light.svg';
+    image3.src = 'img/undraw_conceptual_idea_light.svg';
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkmode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightmode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
